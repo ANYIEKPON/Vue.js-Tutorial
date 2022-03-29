28c46e40523f1c45d38140b26d011eaa1be87b32
ANYIEKPON <74099297+ANYIEKPON@users.noreply.github.com>
2022-03-30 00:52:02 +0100

    vuejs-1b

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             showbooks: true,
             title:'The final Movie',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
 
@@ -17,6 +19,10 @@ const app = Vue.createApp({
             if(data){
                 console.log(data);
             }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
  });
